da5df9ea09003a40bfc36d01d36967c3ee9eaaae
andriusmw <andriusmw@yahoo.es>
2022-07-08 15:59:11 +0200

    router 1

---
+++ metalter/src/components/Footer.js
@@ -0,0 +1,5 @@
+export const Footer = () => {
+    return (
+        <p>Metalter @2022</p>
+    )
+}
---
+++ metalter/src/components/Header.js
@@ -0,0 +1,12 @@
+import { Auth } from "./Auth"
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>\m/ MetalTer \m/</h1>
+            <nav>
+                <Auth />
+           </nav>
+        </header>
+    )
+}
